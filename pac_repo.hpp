c0df1c22673719e4f34a03217fee3b7632876622
_Cicada <2821006329@qq.com>
2021-02-08 15:24:49 +0800

    add: pac_repo & command

--- pac_repo.hpp
+++ pac_repo.hpp
@@ -2,6 +2,7 @@
 #include<fstream>
 #include<unordered_map>
 #include<set>
+#include<filesystem>
 #include "global.hpp"
 class pac_repo{
 private:
@@ -15,7 +16,7 @@ public:/*公开类*/
     };
 
 private:
-    std::unordered_map<std::string,pac_data >local_pac;
+    std::unordered_map<std::string, pac_data>local_pac;
 
 public:/*公开接口*/
     pac_repo(const context *&cxt){
@@ -44,21 +45,42 @@ public:/*公开接口*/
     }
     void update_uninstall(const std::string &name, const std::string &ver){
         auto it = local_pac[name].ver.find(ver);
-        if(local_pac.count(name)==0 || it==local_pac.[name].ver.end())
+        if(local_pac.count(name)==0 || it==local_pac[name].ver.end())
             throw std::invalid_argument("package \""+name+" "+ver+"\" is not existed.");
         if(local_pac[name].available==ver)
             local_pac[name].available.clear();
-        local_pac[name].ver.erase();
+        local_pac[name].ver.erase(ver);
         return;
     }
     void update_checkout(){
 
     }
-    inline std::set<std::string> query_contains_ver(){
-
+    inline std::set<std::string> query_contains_ver(const std::string &name){
+        std::set<std::string> query_reasult;
+        if(local_pac.count(name) != 0){
+            for(auto ver : local_pac[name].ver)
+                query_reasult.insert(ver);
+        }
+        return query_reasult;
     }
     inline std::string query_using_ver(const std::string &name){
-
+        return local_pac.count(name) == 0 ? "" : /*likely*/ local_pac[name].available;
     }
+    // inline std::get_current_runtime_ver(){
+    //     return cxt->runtime_ver;
+    // }
 
+    void uninstall_certain_pac(std::string& name, std::string& ver, context *cxt){
+        std::filesystem::path target(cxt->pac_repo + '/' + name);
+        if(!std::filesystem::exists(target))    //  target 路径不正确
+            throw std::runtime_error("package: " + name + " " + ver + " is not found in .../pac_repo, unstall failed");
+        try {
+            std::filesystem::remove_all(target);    //删除目标路径下所有文件, 这个函数的返回值是成功删除的个数
+        }
+        catch(const std::exception& e) {
+             std::cerr << e.what() << '\n';  //删除失败可能是包正在被使用
+        }
+        throw std::runtime_error("uninstall pack" + name + " " + ver + " failed, please check whether it's using by other progress");
+        return;
+    }
 };
